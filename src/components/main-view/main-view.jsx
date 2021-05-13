ed5ad543c8ef5ed989436bb2cc37627d854abf4e
Brandon Nguyen <51946258+nguyenbrandon5303@users.noreply.github.com>
2021-05-13 11:38:57 -0700

    used react-bootstrap to wrap content in main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,6 +6,9 @@ import { RegistrationView } from '../register-view/register-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default class MainView extends React.Component {
 
   constructor() {
@@ -54,17 +57,29 @@ export default class MainView extends React.Component {
     // If user is not logged in, LoginView will be rendered, otherwise user details are passed to LoginView
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
-    // if (user)
+    if (register) return <RegistrationView onRegistered={register => this.onRegistered(register)} />;
 
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
         {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
-          ))
+          ? (
+            <Row className="justify-content-md-center">
+              <Col md={8}>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            </Row>
+          )
+          : (
+            <Row className="justify-content-md-center">
+              {movies.map(movie => (
+                <Col md={3}>
+                  <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie); }} />
+                </Col>
+              ))}
+            </Row>
+          )
         }
       </div>
     );
